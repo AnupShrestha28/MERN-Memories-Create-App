4cbe5e151e10c8cfb78f3967cc42c919da1d932c
AnupShrestha28 <anupstha0168@gmail.com>
2023-07-06 12:12:41 +0545

    Basic component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,36 @@
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.png";
+import Posts from "./components/posts/Posts";
+import Form from "./components/form/Form";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,12 @@
+import Post from "./post/Post"
+
+const Posts = () => {
+  return (
+    <>
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default Posts
